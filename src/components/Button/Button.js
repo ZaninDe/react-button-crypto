8dfcfbebc8e8016c1559027879ca897aaa2892e5
ZaninDe <gabriel.zaninde@gmail.com>
2022-02-28 12:08:04 -0300

    return correct button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import './button.css'
+import React, { useState } from 'react';
+import './button.css';
+
 
 export const Button = () => {
-  return (
-    <button>Clain</button>
-    )
-}
+  const time = 60 * 60 * 24;
+  const [count, setCount] = useState(0);
+
+  function click() {
+    setCount(time);
+    console.log('click');
+  }
+  
+  return (count == 0 ? (
+    <button onClick={() => (setCount(time), console.log('click'))}>Clain</button>
+  ) : (
+    <button onClick={() => console.log('click')} disabled="disabled">
+      {count}
+    </button>
+  ))
+};
